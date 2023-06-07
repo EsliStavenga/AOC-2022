2422a97701eceeb157d25a44e3082f04d5505d81
Esli Stavenga <esli@mantaraymedia.co.uk>
2023-06-07 22:18:50 +0100

    Day 4

---
+++ day4/1.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once('../shared/InputManager.php');
+
+$data = (new InputManager())->loadLinesForDay();
+$pairs = 0;
+
+foreach($data as $line) {
+  $elfPair = array_map(function(string $range): array {
+    $elfRange = explode('-', $range);
+    $range = range($elfRange[0], $elfRange[1]);
+
+    if(count($range) === 1) {
+      $range[] = $range[0];
+    }
+
+    return $range;
+  }, explode(',', $line));
+
+  usort($elfPair, function(array $a, array $b): int {
+    if($a[0] < $b[0] || end($a) > end($b)) {
+      return -1;
+    }
+
+    return 1;
+  });
+
+  [$firstElf, $secondElf] = $elfPair;
+  if($firstElf[0] <= $secondElf[0] && end($firstElf) >= end($secondElf)) {
+    $pairs++;
+  }
+}
+
+echo $pairs;
